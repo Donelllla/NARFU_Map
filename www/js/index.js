b327163428c09eb2be11b2cf58f957f09927f5f5
BuildTools <unconfigured@null.spigotmc.org>
2024-06-16 18:49:39 +0300

    Добавил загрузку на телефонах

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,7 @@
-document.addEventListener('deviceready', function() { // не работает ХЗ как пофиксить(надо тестить на телефоне)
+document.addEventListener('deviceready', function() {
     console.log("Device is ready!");
     document.querySelector('.footer-text').innerHTML = "Успех";
-    var ref = cordova.InAppBrowser.open('main_screen.html', '_blank', 'location=no');
+    location.href = "main_screen.html";
 }, false);
 
 
